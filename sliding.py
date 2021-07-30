7c0d0eccb822dd300fd07b01c634d59e7fdb1684
Ankitakatoch1991 <88212963+Ankitakatoch1991@users.noreply.github.com>
2021-07-30 14:05:20 -0700

    Add files via upload

---
+++ sliding.py
@@ -0,0 +1,19 @@
+def duplicate(str):
+    dict = {}
+    str1 = ""
+
+    for i in range(len(str)):
+        if str[i] in dict:
+            dict[str[i]] += 1
+        else:
+            dict[str[i]] = 1
+    print(dict)
+
+    for i in dict:
+        if dict[i] > 1:
+            str1 += i
+    print(str1)
+
+duplicate("ankita katoch")
+
+
